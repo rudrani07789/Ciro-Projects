6d56b25e80d1860225235115b3c97d5b540abb4a
rudrani07789 <145859836+rudrani07789@users.noreply.github.com>
2023-11-30 09:38:57 +0530

    Add files via upload

---
+++ rudrani-mondal05-ME_QTRIPDYNAMIC-master/README.md
@@ -0,0 +1,2 @@
+# rudrani-mondal05-ME_QTRIPDYNAMIC
+Crio Repository for user rudrani-mondal05
---
+++ rudrani-mondal05-ME_QTRIPDYNAMIC-master/frontend/conf/index.js
@@ -0,0 +1,4 @@
+
+const config = { backendEndpoint: "http://3.6.89.232:8082" };
+
+export default config;
